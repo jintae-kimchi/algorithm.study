4063dc88217cd5bfcc1fe609a115d88ef36a3284
jintae-kimchi <jintae.kimchi@gmail.com>
2021-11-22 17:21:56 +0900

    [LeetCode] Sort_Array_By_Parity

--- platform/LeetCode/Arrays_101/05_In-Place_Operations/Sort_Array_By_Parity.js
+++ platform/LeetCode/Arrays_101/05_In-Place_Operations/Sort_Array_By_Parity.js
@@ -22,11 +22,28 @@ Note:
  * @param {number[]} A
  * @return {number[]}
  */
- var sortArrayByParity = function(A) {
+var sortArrayByParity = function (A) {
     // 짝수, 홀수 순서로 정렬
     // 요소 값을 정렬하진 않음
     var evenArr = A.filter((num) => num % 2 === 0);
     var oddArr = A.filter((num) => num % 2 === 1);
-    
+
     return evenArr.concat(oddArr);
 };
+
+// 이것도 빠르진 않네..
+// 다른 배열에 담는식으로 하면 훨씬 빠른듯
+// 교환으로 해결하는게 목적이니까 이걸로 넘어감
+var sortArrayByParity_refactored = function (A) {
+    // 교환하는 방식으로 만들어야 함
+    let head = 0;
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] % 2 === 0) {
+            [A[i], A[head]] = [A[head], A[i]];
+            head++;
+        }
+    }
+    return A;
+};
+
+sortArrayByParity_refactored([3, 1, 2, 4]);
