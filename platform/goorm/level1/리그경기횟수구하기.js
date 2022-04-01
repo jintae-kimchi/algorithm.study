ab2643903d455f5dcc76eb3fcc364030929ae67f
jintae-kimchi <jintae.kimchi@gmail.com>
2022-04-01 14:04:40 +0900

    [goorm] 200제 쉬운거 몇개

--- platform/goorm/level1/리그경기횟수구하기.js
+++ platform/goorm/level1/리그경기횟수구하기.js
@@ -1,4 +1,12 @@
 // Run by Node.js
+/**
+ * 1: -
+ * 2: 1
+ * 3: 2 + 1
+ * 4: 3 + 2 + 1
+ * 5: 4 + 3 + 2 + 1
+ * n: (n-1) + (n-2) + ... + 1
+ */
 const solution = (teams) => {
     let ans = 0;
     for (let i = 1; i < teams; i++) {
